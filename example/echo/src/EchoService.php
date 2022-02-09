88ca01fd029caaf8dcbcfa93be40a990b47c3e0b
Rauan Mayemir <rauan@mayemir.io>
2022-02-09 13:56:04 +0300

    Update example service to crash with exception

--- example/echo/src/EchoService.php
+++ example/echo/src/EchoService.php
@@ -11,6 +11,10 @@ class EchoService implements EchoInterface
 {
     public function Ping(ContextInterface $ctx, Message $in): Message
     {
+        throw \Spiral\RoadRunner\GRPC\Exception\InvokeException::create(
+            'Internal',
+            \Spiral\RoadRunner\GRPC\StatusCode::INTERNAL
+        );
         $out = new Message();
 
         return $out->setMsg(date('Y-m-d H:i:s').': PONG');
